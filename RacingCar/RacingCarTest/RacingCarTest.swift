abf77d19f89d710de1e303e63ecce18db8639eab
kyler.jeon <kyler.jeon@kinemaster.com>
2019-09-10 20:50:24 +0900

    개선하기-WIP

---
+++ RacingCar/RacingCarTest/RacingCarTest.swift
@@ -0,0 +1,56 @@
+//
+/******************************************************************************
+ * File Name        : RacingCarTest.swift
+ * Description      : RacingCar
+ *******************************************************************************
+ * Copyright (c) 2002-2019 KineMaster Corp. All rights reserved.
+ * https://www.kinemastercorp.com/
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ ******************************************************************************/
+
+import XCTest
+
+class RacingCarTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    //RacingGame
+    //init(numberOfCar: Int) {
+    //mutating func playOneRound() {
+    //static let MoveOrNot = 4
+    //func needToMove(random: Int) -> Bool {
+    //mutating func playGame(times: Int) {
+    //mutating func run() {
+
+    //Car
+    //init(step: Int = 1) {
+    //mutating func playOneRound(canMove: Bool) {
+    //func printStep(_ step: Int) -> String {
+    //func printStepHistory(at: Int) -> String {
+    
+    //output
+    //static func detectRoundCount(car: Car?) -> Int
+    //static func printOneRound(at index: Int, cars: [Car])
+}
